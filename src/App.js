46c7825cdd5c2ed010338c91b53952e21030f9be
volkanozkann35 <volkanozkann35@gmail.com>
2025-07-21 23:16:07 +0300

    src/App.js dosyası eklendi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>YourBrand Panel</h1>
+      <p>Hoşgeldiniz! Kart üretim ve ekip paneli hazır.</p>
+    </div>
+  );
+}
+
+export default App;
